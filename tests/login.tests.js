6b779bb7e0c46946f3cc9765543ff9c50be67144
Gilson Rodrigues <gilson_rodrigues@hotmail.com>
2025-07-26 22:04:37 -0300

    Adicionando Stages ao teste

--- tests/login.tests.js
+++ tests/login.tests.js
@@ -3,11 +3,17 @@ import { sleep, check } from 'k6';
 
 
 export const options = {
-    vus: 10, 
-    duration: '30s',
+    stages: [
+        { duration: '10s', target: 10 },
+        { duration: '20s', target: 10 },
+        { duration: '10s', target: 30 },
+        { duration: '20s', target: 30 },
+        { duration: '20s', target: 0 },
+
+    ],
     thresholds: {
         http_req_duration: ['p(90)<3000', 'max<5000'],
-        http_req_failed: ['rate<0.01'], 
+        http_req_failed: ['rate<0.01'],
     },
 };
 
@@ -27,10 +33,10 @@ export default function () {
 
     const res = http.post(url, payload, params);
 
-    check(res,{
-        'Validar que o Status é 200' : (r) => r.status === 200,
-        'Validar que o Token é string' : (r) => typeof(r.json().token) == 'string',
-        
+    check(res, {
+        'Validar que o Status é 200': (r) => r.status === 200,
+        'Validar que o Token é string': (r) => typeof (r.json().token) == 'string',
+
     })
 
     sleep(1);
